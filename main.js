0b925619a2f17f45ea00e220d48e663a1732c8d1
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-01-20 10:02:00 -0600

    started vue app

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({ 
+    // new Vue creates a new Vue instance - 
+    // within it is an "OPTIONS" object with optional properties 
+    // properties can store data and perform actions
+    el: '#app',
+    // element property, app connects to div with id="app"
+    data: {
+        product: "Socks"
+    }
+})
