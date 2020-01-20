b8241fc87e6ca6c5e670632c73f3ecbbf1472327
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-01-20 11:22:37 -0600

    lists product info on DOM

--- main.js
+++ main.js
@@ -5,6 +5,22 @@ var app = new Vue({
     el: '#app',
     // element property, app connects to div with id="app"
     data: {
-        product: "Socks"
+        product: "Socks",
+        image: './greensocks.jpg',
+        inventory: 8,
+        newItem: true,
+        onSale: false,
+        details: ["80% cotton, 20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            }, 
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ],
+        sizes: ["S", "M", "L", "XL"],
     }
 })
